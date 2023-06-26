fb31de2a850b4faf33c97205cce75e0532ae10d2
A-Giordano <andrea.giordano991@gmail.com>
2023-06-26 12:52:54 +0200

    initial commit

---
+++ Function_pinecone.py
@@ -0,0 +1,145 @@
+from langchain.vectorstores import Pinecone
+import pinecone
+
+from langchain import LLMMathChain, FAISS, InMemoryDocstore
+from langchain.agents import initialize_agent, Tool, AgentType
+from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import PromptLayerChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import SystemMessage
+from langchain.utilities import BingSearchAPIWrapper
+from langchain.memory import ConversationBufferMemory, RedisChatMessageHistory
+from langchain.prompts import MessagesPlaceholder
+from custom_combined_memory import CustomCombinedMemory
+import faiss
+from time import time
+from custom_memory import ConversationLTSTMemory
+from langchain.memory import VectorStoreRetrieverMemory
+from langchain.memory import ConversationBufferWindowMemory
+
+import os
+# from dotenv import load_dotenv
+# load_dotenv()
+
+
+def get_agent(namespace):
+    llm = PromptLayerChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+    # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+
+    ##########################################
+
+    llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+    search = BingSearchAPIWrapper()
+
+    search_tool = Tool(
+        name="Search",
+        func=search.run,
+        description="useful for when you need to answer questions about current events or the current state of the world. You should ask targeted questions"
+    )
+
+    calc = Tool(
+        name="Calculator",
+        func=llm_math_chain.run,
+        description="useful for when you need to answer questions about math"
+    )
+    tools = [search_tool, calc]
+
+    ###########################################
+    pinecone.init(environment="us-west1-gcp-free")
+
+    index_name = 'langchain-demo1'
+    if index_name not in pinecone.list_indexes():
+        # we create a new index
+        pinecone.create_index(
+            name=index_name,
+            metric='dotproduct',
+            dimension=1536  # 1536 dim of text-embedding-ada-002
+        )
+
+    index = pinecone.Index(index_name)
+
+    embeddings = OpenAIEmbeddings()
+    vectorstore = Pinecone(index=index,
+                           embedding_function=embeddings.embed_query,
+                           text_key="text",
+                           namespace=namespace,
+                           )
+    # retriever = vectorstore.as_retriever(search_kwargs=dict(k=2))
+
+    retriever = vectorstore.as_retriever(search_type="similarity_score_threshold",
+                                         search_kwargs=dict(k=4,
+                                                            # search_type="mmr",
+                                                            # fetch_k=4,
+                                                            # lambda_mult=0.2,
+                                                            score_threshold=0.4
+                                                            ))
+    #############################################
+
+    message_history = RedisChatMessageHistory(url='redis://localhost:6379/0', ttl=600, session_id=namespace)
+    memory = ConversationLTSTMemory(memory_key="memory",
+                                    k=2,
+                                    # human_prefix="Human",
+                                    # ai_prefix="AI",
+                                    chat_memory=message_history,
+                                    long_term_retriever=retriever,
+                                    input_key="input",
+                                    return_messages=True)
+
+    # memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+        "system_message": SystemMessage(content="You are Steve Jobs, always reply as him.")
+    }
+    agent_chain = initialize_agent(tools,
+                                   llm,
+                                   agent=AgentType.OPENAI_FUNCTIONS,
+                                   agent_kwargs=agent_kwargs,
+                                   memory=memory,
+                                   max_execution_time=10,
+                                   verbose=False)
+
+    return agent_chain
+
+# agent_chain  = get_agent('usertest1')
+
+# start = time()
+# print(agent_chain.run(input="hi my name is bob and i'm a software developer"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="what's your name?"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="who is maradona?"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="who is the prime minister of italy?"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="how old is she?"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="how old is the oldest between the french and american prime minister?"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="who won the last world cup?"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="where is the capital of italy?"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="what's my job?"))
+# print(f"elapsed time: {time() - start}")
+#
+# start = time()
+# print(agent_chain.run(input="what's my name?"))
+# print(f"elapsed time: {time() - start}")
+# pass
---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from langchain import PromptTemplate, OpenAI, LLMChain
+import chainlit as cl
+from Function_pinecone import get_agent
+import uuid
+
+
+@cl.langchain_factory(use_async=True)
+def factory():
+    # prompt = PromptTemplate(template=template, input_variables=["question"])
+    # llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
+
+    return get_agent(namespace=str(uuid.uuid4()))
