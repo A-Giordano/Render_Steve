661c1d790a49d0ad4bb114c29521f3db3287ee71
A-Giordano <andrea.giordano991@gmail.com>
2023-08-31 16:32:13 +0200

    update

--- Function_pinecone.py
+++ Function_pinecone.py
@@ -260,7 +260,7 @@ def get_agent(namespace):
     pinecone.init(environment="eu-west4-gcp")
     # pinecone.init(environment="gcp-starter")
 
-    index_name = 'chainlit_demo'
+    index_name = 'chainlit-demo'
     if index_name not in pinecone.list_indexes():
         # we create a new index
         pinecone.create_index(
